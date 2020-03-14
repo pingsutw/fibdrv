d421fd8b193040d4879fb5a117105d76a501e463
pingsutw <pingsutw@gmail.com>
2020-03-15 05:04:02 +0800

    Implement a adder to add two unsigned long long number

--- client.c
+++ client.c
@@ -7,11 +7,59 @@
 
 #define FIB_DEV "/dev/fibonacci"
 
+#define MAX_LENGTH 100
+
+void big_fibnum(char *buf, long long sz)
+{
+    char lower[MAX_LENGTH] = {0};
+    snprintf(lower, sizeof(lower), "%s", buf);
+
+    long long upper = sz;
+    int carry = 0;
+
+    char scale[MAX_LENGTH] = {0};
+    snprintf(scale, sizeof(scale), "%s", "18446744073709551616");
+
+    for (int i = 0; i < strlen(lower) / 2; i++) {
+        char tmp = lower[i];
+        lower[i] = lower[strlen(lower) - 1 - i];
+        lower[strlen(lower) - 1 - i] = tmp;
+    }
+
+    for (int i = 0; i < strlen(scale) / 2; i++) {
+        char tmp = scale[i];
+        scale[i] = scale[strlen(scale) - 1 - i];
+        scale[strlen(scale) - 1 - i] = tmp;
+    }
+
+    for (int i = 0; i < MAX_LENGTH; i++) {
+        if ((upper == 0 || scale[i] == '\0') && lower[i] == '\0' &&
+            carry == 0) {
+            break;
+        }
+        int x = 0, y = 0;
+        if (scale[i] != '\0')
+            x = upper * (scale[i] - '0');
+        if (lower[i] != '\0')
+            y = (lower[i] - '0');
+        int tmp = x + y + carry;
+        buf[i] = (char) (tmp % 10 + '0');
+        carry = tmp / 10;
+    }
+
+    for (int i = 0; i < strlen(buf) / 2; i++) {
+        char tmp = buf[i];
+        buf[i] = buf[strlen(buf) - 1 - i];
+        buf[strlen(buf) - 1 - i] = tmp;
+    }
+    return;
+}
+
 int main()
 {
     long long sz;
 
-    char buf[1];
+    char buf[100];
     char write_buf[] = "testing writing";
     int offset = 100; /* TODO: try test something bigger than the limit */
 
@@ -29,19 +77,21 @@ int main()
     for (int i = 0; i <= offset; i++) {
         lseek(fd, i, SEEK_SET);
         sz = read(fd, buf, 1);
+        big_fibnum(buf, sz);
         printf("Reading from " FIB_DEV
                " at offset %d, returned the sequence "
-               "%lld.\n",
-               i, sz);
+               "%s.\n",
+               i, buf);
     }
 
     for (int i = offset; i >= 0; i--) {
         lseek(fd, i, SEEK_SET);
         sz = read(fd, buf, 1);
+        big_fibnum(buf, sz);
         printf("Reading from " FIB_DEV
                " at offset %d, returned the sequence "
-               "%lld.\n",
-               i, sz);
+               "%s.\n",
+               i, buf);
     }
 
     close(fd);
--- fibdrv.c
+++ fibdrv.c
@@ -6,6 +6,8 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/mutex.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
 
 MODULE_LICENSE("Dual MIT/GPL");
 MODULE_AUTHOR("National Cheng Kung University, Taiwan");
@@ -17,7 +19,7 @@ MODULE_VERSION("0.1");
 /* MAX_LENGTH is set to 92 because
  * ssize_t can't fit the number > 92
  */
-#define MAX_LENGTH 92
+#define MAX_LENGTH 100
 
 static dev_t fib_dev = 0;
 static struct cdev *fib_cdev;
@@ -26,21 +28,32 @@ static DEFINE_MUTEX(fib_mutex);
 
 struct BigN {
     unsigned long long lower, upper;
-} fib_number;
+};
 
+static inline void addBigN(struct BigN *output, struct BigN x, struct BigN y)
+{
+    output->upper = x.upper + y.upper;
+    if (y.lower > ~x.lower)
+        output->upper++;
+    output->lower = x.lower + y.lower;
+}
 
-static long long fib_sequence(long long k)
+static struct BigN fib_sequence(long long k)
 {
     /* FIXME: use clz/ctz and fast algorithms to speed up */
-    long long f[k + 2];
+    struct BigN f[k + 2];
 
-    f[0] = 0;
-    f[1] = 1;
+    f[0].upper = 0;
+    f[0].lower = 0;
+
+    f[1].upper = 0;
+    f[1].lower = 1;
 
     for (int i = 2; i <= k; i++) {
-        f[i] = f[i - 1] + f[i - 2];
+        addBigN(&f[i], f[i - 1], f[i - 2]);
     }
 
+    // printk("%lld , %llu , %llu", k, f[k].upper, f[k].lower);
     return f[k];
 }
 
@@ -65,7 +78,11 @@ static ssize_t fib_read(struct file *file,
                         size_t size,
                         loff_t *offset)
 {
-    return (ssize_t) fib_sequence(*offset);
+    char kbuffer[100] = {0};
+    struct BigN result = fib_sequence(*offset);
+    snprintf(kbuffer, sizeof(kbuffer), "%llu", result.lower);
+    copy_to_user(buf, kbuffer, 100);
+    return result.upper;
 }
 
 /* write operation is skipped */
